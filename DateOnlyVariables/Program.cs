8820d46db1c0b443c30b2799ce96463a6951ceb6
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:38:23 +0200

    add app

---
+++ DateOnlyVariables/Program.cs
@@ -0,0 +1,11 @@
+﻿
+DateTime today = DateTime.Now;
+
+DateOnly birthday = DateOnly.Parse("11/6/1998");
+
+Console.WriteLine(birthday.ToString("MMMM dd, yyyy"));
+
+
+Console.WriteLine($"Today full format: {today.ToString("d/MM/yy, h:mm tt K")}");
+Console.WriteLine($"Today just date: {today.Date}");
+Console.WriteLine($"Birthday full format: {birthday}");
